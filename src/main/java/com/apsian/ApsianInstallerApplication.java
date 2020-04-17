34132153b32ecb4b1d8a3746e2e5b679c2523be1
GoastRider5 <nandish.1735@gmail.com>
2020-04-17 17:47:51 +0530

    project initialization

---
+++ src/main/java/com/apsian/ApsianInstallerApplication.java
@@ -0,0 +1,13 @@
+package com.apsian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApsianInstallerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApsianInstallerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apsian/ApsianInstallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apsian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsianInstallerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
