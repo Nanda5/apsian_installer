4063ca1291e4d65e9ed652c1db58006c5c281e37
Prakash G <pg@gh.pri>
2020-04-18 04:50:41 +0000

    INstaller Implementation

--- src/main/java/com/apsian/ApsianInstallerApplication.java
+++ src/main/java/com/apsian/ApsianInstallerApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApsianInstallerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ApsianInstallerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ApsianInstallerApplication.class, args);
+
+	}
 
 }
